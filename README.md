07232163ecbeab746db9e36a7d63af6cef5f3be0
Jandrus000 <jcandrus02@gmail.com>
2024-03-08 17:37:22 -0700

    Finished log-in page

---
+++ README.md
@@ -0,0 +1,33 @@
+# Overview
+
+
+
+This web app is a simple demonstration of a log-in/ sign-up page. This app communicates with FireBase real time database to store users. If users are created or found in the database they are permitted to see the main page.
+
+This purpose of this app was to learn how to use a cloud database and how to implement it using JavaScript. It was also overall practice for JavaScript, HTML, and CSS.
+
+[Software Demo Video](https://youtu.be/ww8SeC2PihY)
+
+# Cloud Database
+
+I am using FireBase real time Database
+
+"users" is the parent item in the database and each child is added with their username being the unique key for each. Each username has the child elements of disliked_movie, favorite_food, name, and password,
+
+# Development Environment
+
+This project was developed inside of Visual Studio Code
+
+JavaScript was used to implement Firebase and all other functionality
+
+# Useful Websites
+
+- [W3 Schools](https://www.w3schools.com/)
+- [stack overflow](https://stackoverflow.com/)
+- [Firebase Documentation](https://firebase.google.com/docs/)
+
+# Future Work
+
+- The database is not secure. If I were to want to keep passwords or any sensitive information I would have to find a way to make the database more secure.
+- I want to display user information on the main page. 
+- I want to improve the styling of each page
--- app.js
+++ app.js
@@ -2,8 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getDatabase,ref,child,get,set } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
 
-
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC7PsVP0HuVHKgdUoQnG1xx91H_QFxCiWE",
   authDomain: "log-in-page-bfa67.firebaseapp.com",
@@ -12,21 +11,90 @@ const firebaseConfig = {
   messagingSenderId: "378471433412",
   appId: "1:378471433412:web:9729f3be117ab6dca0f591"
 };
+const firebase = initializeApp(firebaseConfig);
+var database = getDatabase(firebase);
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-const db = getDatabase(app);
-
-
-
+//check to see if page is loaded
 document.addEventListener("DOMContentLoaded", edvent => {
-    document.getElementById("name_submit").addEventListener('click',function(e){
-        set(ref(db, '/user',document.getElementById('name_box').value),
-        {
-            name: document.getElementById('name_box').value
-        })
-        alert("log-in success");
-    });
+    
+    //sign up page finish button
+    var element = document.getElementById("finish_button")
+
+    //check if button exists
+    if(element){
+        element.addEventListener('click',function(e){
+            
+            let name = document.getElementById('create_name').value;
+            let password = document.getElementById('create_password').value;
+            let check_password = document.getElementById('create_checkpassword').value;
+            let food = document.getElementById('create_food').value;
+            let movie = document.getElementById('create_movie').value;
+    
+            const itemRef = ref(database, 'users/' + name);
+            
+            //check if all fields are filled out
+            if(name == null || name == "" || password == null || password == "" ||
+                check_password == null || check_password == "" ||food == null || food == "" ||
+                movie == null || movie == "" )
+            {
+                alert("Please fill out all fields");
+                //check if password matches password check
+            } else if (password != check_password){
+                alert("Your passwords don't match");
+                document.getElementById('create_password').value="";
+                document.getElementById('create_checkpassword').value="";
+            }else 
+            {
+                get(itemRef).then((snapshot) => {
+                    //check if user already exists
+                    if(snapshot.exists()){
+                        alert("\""+name+"\" is a user that already exists!");
+                    }
+                    //create user in database
+                    else{
+                        set(ref(database, 'users/'+ name),{
+                            name: name,
+                            password: password,
+                            favorite_food: food,
+                            disliked_movie: movie
+                        }).then(function() {
+                            //load main_page
+                            window.location.href = "main_page.html"
+                        });
+                    }
+                });
+                
+            }
+            
+        });
+    }
+    
+
+    //log in page submit
+    element = document.getElementById("submit_button")
+    //check if submit button exists
+    if(element){
+        element.addEventListener('click',function(e){
+            let name = document.getElementById('name_box').value;
+            let entered_password = document.getElementById('password_box').value;
+
+            //check to see if user exists in database
+            const userRef = ref(database, 'users/'+name+'/password');
+            get(userRef).then((snapshot) => {
+                if(snapshot.exists()){
+                    const password = snapshot.val();
+                    //check if password is valid
+                    if(entered_password == password){
+                        window.location.href = "main_page.html";
+                    }else{
+                        alert("Your username or password does not match, please try again or sign up.")
+                    }
+                }else{
+                    alert("Your username or password does not match, please try again or sign up.")
+                }
+            })
+        });
+    }
+    
 });
 
